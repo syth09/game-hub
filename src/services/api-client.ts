9c4748d48c7e99b0bbf44ababfd7750fcf4b6156
syth09 <tatuanphong616@gmail.com>
2023-11-10 23:16:17 +0700

    Build the color mode switch and setup the game fetching data

---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+        baseURL: 'https://api.rawg.io/api/games',
+        params: {
+                key: '9eae00bfd5b94fa5859c69d91e6d5e7d'
+        }
+})
